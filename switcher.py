7e89c5b17fa9b1a2e3a553c8b121b1f0b0ac5431
Sasha <srush.research@gmail.com>
2024-06-18 11:37:21 -0400

    .

--- switcher.py
+++ switcher.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+from enum import Enum
 from dataclasses import dataclass
 from contextlib import contextmanager
 import array_api_compat
@@ -6,25 +7,30 @@ import array_api_compat
 if TYPE_CHECKING:
     from namespace import _ArrayAPINameSpace
 else:
-    import numpy.array_api as _ArrayAPINameSpace
-from array_
+    import array_api_compat.numpy as _ArrayAPINameSpace
 
-class XP(enum):
+
+class XP(Enum):
     Numpy = 1
     Jax = 2
+    Pytorch = 3
 
 @dataclass
 class _Array:
-    mode: 
+    mode: XP
     @property
     def xp(self):
-        if self.mode == Numpy:
+        if self.mode == XP.Numpy:
             import array_api_compat.numpy as onp
             return onp 
-        elif self.mode == Jax:
-            import array_api_compat.pytorch as pnp
+        elif self.mode == XP.Pytorch:
+            import jax.experimental.array_api as jnp
+            return jnp
+        elif self.mode == XP.Jax:
+            import array_api_compat.pytroch as pnp
             return pnp
         
+    @contextmanager
     def set_context(self, typ: XP):
         # Code to acquire resource, e.g.:
         old_mode = typ
@@ -33,5 +39,6 @@ class _Array:
             yield None
         finally:
             # Code to release resource, e.g.:
-            self.mode(old_mode)
+            self.mode = old_mode
 
+switch = _Array(XP.Numpy) 
